9132fd5d928622524ec0d9ee53328f868e1494f5
Seiichi Inoue <d35inou108@gmail.com>
2019-10-25 09:48:26 +0900

    made common define and include

---
+++ src/common.hpp
@@ -0,0 +1,16 @@
+#include <unordered_map>
+#include "hashmap.hpp"
+template<class T, class U>
+// using hashmap = std::unordered_map<T, U>;
+using hashmap = emilib::HashMap<T, U>;
+
+#define HPYLM_INITIAL_D 0.5
+#define HPYLM_INITIAL_THETA 2.0
+#define HPYLM_INITIAL_A 1.0
+#define HPYLM_INITIAL_B 1.0
+#define HPYLM_INITIAL_ALPHA 1.0
+#define HPYLM_INITIAL_BETA 1.0
+
+using id = size_t;
+#define ID_BOS 0
+#define ID_EOS 1
