a3a87823ed4e14ade8ac40f77e0683b50532bebd
Seiichi Inoue <d35inou108@gmail.com>
2019-10-29 23:43:42 +0900

    support japanese courpus

--- utils/generate.py
+++ utils/generate.py
@@ -7,9 +7,12 @@ def generate(args):
     hpylm.load(args.model)
     for _ in range(args.num):
         print(hpylm.generate_sentence())
+        # for japanese
+        # generated = hpylm.generate_sentence().replace(" ", "")
+        # print(generated)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("-n", "--num", default=1)
+    parser.add_argument("-n", "--num", type=int, default=1)
     parser.add_argument("-m", "--model", default="./model")
     generate(parser.parse_args())
